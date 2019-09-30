44758ba022f5d5900397b4fe2dd5f1eb79b17cfe
baigx-git <723241017@qq.com>
2019-09-30 10:40:01 +0800

    ceshi
    ceshi

--- index/index.js
+++ index/index.js
@@ -8,4 +8,24 @@ Page({
     console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
     console.log('https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/devtools.html')
   },
+  clickMe: function () {
+    this.setData({ msg: "Hello World" })
+    
+  },
+   handleTap1: function () {
+     this.setData({ msg: "handleTap1" })
+     console.log(this.data);
+  },
+   handleTap2: function () {
+     this.setData({ msg: "handleTap2" })
+     console.log(this.data);
+  },
+   handleTap3: function () {
+     this.setData({ msg: "handleTap3" })
+     console.log(this.data);
+  },
+   handleTap4: function () {
+     this.setData({ msg: "handleTap4" })
+     console.log(this.data);
+  }
 })
